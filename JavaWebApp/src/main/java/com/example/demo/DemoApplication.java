fea4df728d8643df62b8ceb08a1dd336121d35e7
cditua <cditua@gmail.com>
2022-10-25 21:13:31 -0400

    fix

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 
-		String name ="charlie"
+		String name = "charlie";
 		System.out.println(name);
 	}
 
